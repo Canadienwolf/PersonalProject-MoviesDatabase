8c53428b89fc49987b0df177d215e0174f19356f
Frank Lindvik <frank.lindvik@cillco.com>
2022-09-21 15:59:05 +0200

    CommitDay3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MediaItemListComponent } from './media-item-list/media-item-list.component';
+import { MediaItemComponent } from './media-item/media-item.component';
+import { FavoriteDirective } from './favorite.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemListComponent,
+    MediaItemComponent,
+    FavoriteDirective
   ],
   imports: [
     BrowserModule,
